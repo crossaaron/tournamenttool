b7dda49620f200f4f44ff97fb742f30de68bcfae
crossaaron <atcbcrossx@gmail.com>
2019-03-08 12:01:51 -0800

    project structure

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div id='app-container'>
+                <h1>React Template</h1>
+                <p> Magic starts here</p>
+            </div>
+        )
+
+    }
+}
